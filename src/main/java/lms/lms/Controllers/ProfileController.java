43296b83763e11928b5d5db429058d94c331b919
Isaac Yanez <isaac231467@gmail.com>
2023-04-01 22:34:49 -0500

    forgot to checkout to a new branch

--- src/main/java/lms/lms/Controllers/ProfileController.java
+++ src/main/java/lms/lms/Controllers/ProfileController.java
@@ -1,13 +1,20 @@
 package lms.lms.Controllers;
 
 
+import lms.lms.Models.User;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProfileController {
     @GetMapping("/profile")
-    public String HomeController(){
+    public String HomeController(Model model){
+        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("user", loggedInUser);
         return "Profile/Profile";
     }
+
+
 }
