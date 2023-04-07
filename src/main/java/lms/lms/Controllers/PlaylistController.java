5918497d0f027530fa2dd4cba1abc9b747b15a45
Isaac Yanez <isaac231467@gmail.com>
2023-04-07 15:32:07 -0500

    was able to create a view for a single playlist so the user can see the videos that are in the specefied playlist

--- src/main/java/lms/lms/Controllers/PlaylistController.java
+++ src/main/java/lms/lms/Controllers/PlaylistController.java
@@ -2,11 +2,17 @@ package lms.lms.Controllers;
 
 
 import lms.lms.Models.Playlist;
+import lms.lms.Models.PlaylistVideo;
 import lms.lms.Models.User;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class PlaylistController {
@@ -17,6 +23,15 @@ public class PlaylistController {
     public PlaylistController(PlaylistRepository playlistDao, UserRepository userDao){
         this.playlistDao = playlistDao;
         this.userDao = userDao;
+    }
+
+    @GetMapping("/playlistview/{id}")
+    public String showPlaylistView(@PathVariable("id") Long id, Model model){
+        Playlist playlist = playlistDao.findById(id).get();
+        List<PlaylistVideo> playlistVideos = playlist.getPlaylistVideos();
+        model.addAttribute("videos", playlistVideos);
+        model.addAttribute("playlist", playlist);
+        return "playlist/show";
     }
 
     @PostMapping("/playlist/create")
--- src/main/java/lms/lms/SecurityConfiguration.java
+++ src/main/java/lms/lms/SecurityConfiguration.java
@@ -53,6 +53,7 @@ public class SecurityConfiguration {
                 .and()
                 .authorizeHttpRequests()
                 .requestMatchers(
+                        "/playlistview/{id}",
                         "/add-video-to-playlist",
                         "/playlist/create",
                         "/profile",
