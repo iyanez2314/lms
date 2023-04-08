cffef3a20eef33da12d71631635c1d122f394dbc
Isaac Yanez <isaac231467@gmail.com>
2023-04-08 10:07:52 -0500

    was able to delete a video from a specefied playlist

--- src/main/java/lms/lms/Controllers/PlaylistController.java
+++ src/main/java/lms/lms/Controllers/PlaylistController.java
@@ -4,8 +4,11 @@ package lms.lms.Controllers;
 import lms.lms.Models.Playlist;
 import lms.lms.Models.PlaylistVideo;
 import lms.lms.Models.User;
+import lms.lms.Models.Video;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,9 +23,15 @@ public class PlaylistController {
     private final PlaylistRepository playlistDao;
     private final UserRepository userDao;
 
-    public PlaylistController(PlaylistRepository playlistDao, UserRepository userDao){
+    private final PlaylistVideoRepository playlistVideoDao;
+
+    private final VideoRepository videoDao;
+
+    public PlaylistController(PlaylistRepository playlistDao, UserRepository userDao, PlaylistVideoRepository playlistVideoDao, VideoRepository videoDao){
         this.playlistDao = playlistDao;
         this.userDao = userDao;
+        this.playlistVideoDao = playlistVideoDao;
+        this.videoDao = videoDao;
     }
 
     @GetMapping("/playlistview/{id}")
@@ -34,10 +43,27 @@ public class PlaylistController {
         return "playlist/show";
     }
 
+    @PostMapping("/playlistview/{id}")
+    public String editPlaylistView(@PathVariable("id") Long id, @RequestParam("videoId") Long videoId, Model model){
+//        We need to save any update the user did to the playlist.
+//        I need to check if the user changed the playlist name, or removed the videos form the playlist.
+    Playlist playlist = playlistDao.findById(id).get();
+    Video video = videoDao.findById(videoId).get();
+
+    PlaylistVideo playlistVideo = playlistVideoDao.findAll().stream()
+            .filter(pv -> pv.getPlaylist().equals(playlist) && pv.getVideo().equals(video))
+            .findFirst()
+            .orElse(null);
+
+        if (playlistVideo != null) {
+            playlistVideoDao.delete(playlistVideo);
+        }
+        return "redirect:/playlistview/" + id;
+    }
+
     @PostMapping("/playlist/create")
     public String playlistCreation(@RequestParam(name = "playlistname") String playlistname){
         User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        System.out.println("here");
         Long userId = loggedInUser.getId();
         User user = userDao.findById(userId).get();
         Playlist newPlaylist = new Playlist(playlistname);
