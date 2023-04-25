c25a9f5cdcc9f4070bcda0e21915e7f7e3dc45ff
Isaac Yanez <isaac231467@gmail.com>
2023-04-25 12:20:09 -0500

    was able to get the featured videos to appear on the home page

--- seeder.sql
+++ seeder.sql
@@ -16,3 +16,10 @@ VALUES ('Python'),
        ('Swift'),
        ('Kotlin'),
        ('Go');
+
+USE lms_db;
+INSERT INTO featured_videos(thumbnail, title, url)
+VALUES ('https://i.ytimg.com/vi/7LNl2JlZKHA/hqdefault.jpg', 'How to Create a Flask + React Project | Python Backend + React Frontend', 'https://www.youtube.com/watch?v=7LNl2JlZKHA'),
+       ('https://i.ytimg.com/vi/p5AtOUyf250/hqdefault.jpg', 'HOW DOES IT WORK? CONNECT JS FRONTEND AND PYTHON BACKEND TOGETHER', 'https://www.youtube.com/watch?v=p5AtOUyf250'),
+       ('https://i.ytimg.com/vi/Wpeym1_lmPo/hqdefault.jpg', 'Java vs JavaScript', 'https://www.youtube.com/watch?v=Wpeym1_lmPo'),
+       ('https://i.ytimg.com/vi/sQCkdbu5L1c/hqdefault.jpg', 'Difference between Java, PHP, Ruby and Python', 'https://www.youtube.com/watch?v=sQCkdbu5L1c')
--- src/main/java/lms/lms/Controllers/HomeController.java
+++ src/main/java/lms/lms/Controllers/HomeController.java
@@ -8,8 +8,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    private final FeaturedVideoRepository featuredDao;
+
+    public HomeController(FeaturedVideoRepository featuredDao) {
+        this.featuredDao = featuredDao;
+    }
+
     @GetMapping("/")
-    public String showHomeScreen(){
+    public String showHomeScreen(Model model){
+        model.addAttribute("featuredVideos", featuredDao.findAll());
         return "index";
     }
 }
